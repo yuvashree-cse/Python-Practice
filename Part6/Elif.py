07f55ce2a17ff2d1cf031818f92f012a632d160f
Yuvashree Chandra Sekar <yuvashreecv@gmail.com>
2026-08-12 13:19:10 +0530

    day 3

---
+++ Part6/Elif.py
@@ -0,0 +1,9 @@
+score = 70
+if score >= 90:
+    print("Grade: A")
+elif score >= 80:
+    print("Grade: B")
+elif score >= 70:
+    print("Grade: C")
+else:
+    print("Grade: F")
---
+++ Part6/nested.py
@@ -0,0 +1,10 @@
+age = 18
+has_id = True
+
+if age >= 18:
+    if has_id:
+        print("You can vote")
+    else:
+        print("You must have id to vote")
+else:
+    print("Sorry, You can't vote")
